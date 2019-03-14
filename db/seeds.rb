f6ccbff35913fde1f23556f8f6dfcc4edc02e0d3
htokel <hakantokel@gmail.com>
2019-03-14 20:48:34 +0300

    Vay bu hos oldu iste

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Person.destroy_all
+
+Person.create! [
+  {first_name: "Kalman", last_name: "Smith", age: 33},
+  {first_name: "John", last_name: "Whatever", age: 27},
+  {first_name: "Micheal", last_name: "Smith", age: 15},
+  {first_name: "Josh", last_name: "Oreck", age: 57},
+  {first_name: "John", last_name: "Smith", age: 27},
+  {first_name: "Bill", last_name: "Gates", age: 75},
+  {first_name: "LeBron", last_name: "James", age: 30},
+]
